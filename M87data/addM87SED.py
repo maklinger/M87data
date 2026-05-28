940e1e04554f13203da76ac4d753257179faf661
maklinger <marc.klinger@desy.de>
2026-05-28 19:16:01 +0200

    add shorter JWST, separate out more data for plotting

--- M87data/addM87SED.py
+++ M87data/addM87SED.py
@@ -49,7 +49,8 @@ def get_SED_data(dataset="M87SED_EHTMWL2018",
 def add_SED(ax, dataset="M87SED_EHTMWL2018", angular_scale_color=True, 
             cmap = plt.get_cmap("viridis"), D_Mpc=16.7, MBH_MSUN=6.6e9, theta_view_deg=17,
             convert_to_rg=True, colorbar=True, fixed_color="k", marker=".", alphaUL=1,
-            frequencymarkers=True, absorbed=True, ymin=1e-20, ymax=1e-10):
+            frequencymarkers=True, absorbed=True, ymin=1e-20, ymax=1e-10, label=None,
+            alpha=1):
     
     df, df_data, df_VM, df_UL = get_SED_data(dataset, D_Mpc, MBH_MSUN, theta_view_deg)
 
@@ -73,7 +74,10 @@ def add_SED(ax, dataset="M87SED_EHTMWL2018", angular_scale_color=True,
         for i in range(len(df_data)):
             ax.errorbar([df_data["Frequency_Hz"].iat[i]*h*erg2eV], [df_data["nuFnu_1e-12ergscm2"].iat[i]*1e-12], 
                         yerr=[df_data["sigma_nuFnu_1e-12ergscm2"].iat[i]*1e-12], ls="", marker=marker, ms=8,
-                        color=cmap(norm(np.log10(df_data['Angular_Scale_arcsec'].iat[i]*fac_ang_dist_z))))
+                        color=cmap(norm(np.log10(df_data['Angular_Scale_arcsec'].iat[i]*fac_ang_dist_z))),
+                        label=label, alpha=alpha,
+                        zorder=10-np.log10(df_data['Angular_Scale_arcsec'].iat[i]*fac_ang_dist_z))
+            label=None
         for i in range(len(df_UL)):
             ax.errorbar([df_UL["Frequency_Hz"].iat[i]*h*erg2eV], [df_UL["nuFnu_1e-12ergscm2"].iat[i]*1e-12], 
                         yerr=[df_UL["sigma_nuFnu_1e-12ergscm2"].iat[i]*1e-12], uplims=True, ls="", marker="_", 
@@ -85,10 +89,11 @@ def add_SED(ax, dataset="M87SED_EHTMWL2018", angular_scale_color=True,
             plt.colorbar(sm, ax=ax, label=col_label, shrink=0.75, pad=0.01)
     else:
         ax.errorbar(df_data["Frequency_Hz"]*h*erg2eV, df_data["nuFnu_1e-12ergscm2"]*1e-12, 
-                    yerr=df_data["sigma_nuFnu_1e-12ergscm2"]*1e-12, ls="", marker=marker, c=fixed_color)
+                    yerr=df_data["sigma_nuFnu_1e-12ergscm2"]*1e-12, ls="", marker=marker, 
+                    c=fixed_color, label=label, alpha=alpha)
         if dataset=="M87SED_EHTMWL2018":
             ax.errorbar(df_VM["Frequency_Hz"]*h*erg2eV, df_VM["nuFnu_1e-12ergscm2"]*1e-12, 
-                yerr=df_VM["sigma_nuFnu_1e-12ergscm2"]*1e-12, ls="", marker=".", c='grey', alpha=0.3)
+                yerr=df_VM["sigma_nuFnu_1e-12ergscm2"]*1e-12, ls="", marker=".", c='grey', alpha=0.3, label=label)
         ax.errorbar(df_UL["Frequency_Hz"]*h*erg2eV, df_UL["nuFnu_1e-12ergscm2"]*1e-12, 
                     yerr=df_UL["sigma_nuFnu_1e-12ergscm2"]*1e-12,uplims=True, ls="", marker="_", c="grey", alpha=alphaUL)
 
