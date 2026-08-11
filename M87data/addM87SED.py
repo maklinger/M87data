269ded86371005b6a100600ada2e53b634ddcbed
maklinger <marc.klinger@desy.de>
2026-08-11 15:08:30 +0200

    add markersize

--- M87data/addM87SED.py
+++ M87data/addM87SED.py
@@ -48,9 +48,9 @@ def get_SED_data(dataset="M87SED_EHTMWL2018",
 
 def add_SED(ax, dataset="M87SED_EHTMWL2018", angular_scale_color=True, 
             cmap = plt.get_cmap("viridis"), D_Mpc=16.7, MBH_MSUN=6.6e9, theta_view_deg=17,
-            convert_to_rg=True, colorbar=True, fixed_color="k", marker=".", alphaUL=1,
+            convert_to_rg=True, colorbar=True, fixed_color="k", marker=".", ms=6, alphaUL=1,
             frequencymarkers=True, absorbed=True, ymin=1e-20, ymax=1e-10, label=None,
-            alpha=1):
+            alpha=1, cb_shrink=0.75, cb_pad=0.01):
     
     df, df_data, df_VM, df_UL = get_SED_data(dataset, D_Mpc, MBH_MSUN, theta_view_deg)
 
@@ -61,19 +61,20 @@ def add_SED(ax, dataset="M87SED_EHTMWL2018", angular_scale_color=True,
             rg = MBH*G/c**2
             inclination = np.deg2rad(theta_view_deg) 
             fac_ang_dist_z = distance/rg * arcsec2rad / np.sin(inclination)
-            col_label= r'up to $\log_{10}(z/r_g)$'
+            col_label= r'aperture $\log_{10}(z/r_g)$'
             norm = Normalize(vmin=1, vmax=9)
         else:
             fac_ang_dist_z = 1
-            col_label = r'up to $\log_{10}(z/1\,arcsec)$'
+            col_label = r'aperture $\log_{10}(z/1\,arcsec)$'
             norm = Normalize(vmin=np.log10(df_data['Angular_Scale_arcsec'].min()), 
                             vmax=np.log10(df_data['Angular_Scale_arcsec'].max()))
         if dataset=="M87SED_EHTMWL2018":
             ax.errorbar(df_VM["Frequency_Hz"]*h*erg2eV, df_VM["nuFnu_1e-12ergscm2"]*1e-12, 
-                yerr=df_VM["sigma_nuFnu_1e-12ergscm2"]*1e-12, ls="", marker=".", c='grey', alpha=0.3)
+                yerr=df_VM["sigma_nuFnu_1e-12ergscm2"]*1e-12, ls="", marker=".", 
+                c='grey', ms=ms, alpha=0.3)
         for i in range(len(df_data)):
             ax.errorbar([df_data["Frequency_Hz"].iat[i]*h*erg2eV], [df_data["nuFnu_1e-12ergscm2"].iat[i]*1e-12], 
-                        yerr=[df_data["sigma_nuFnu_1e-12ergscm2"].iat[i]*1e-12], ls="", marker=marker, ms=8,
+                        yerr=[df_data["sigma_nuFnu_1e-12ergscm2"].iat[i]*1e-12], ls="", marker=marker, ms=ms,
                         color=cmap(norm(np.log10(df_data['Angular_Scale_arcsec'].iat[i]*fac_ang_dist_z))),
                         label=label, alpha=alpha,
                         zorder=10-np.log10(df_data['Angular_Scale_arcsec'].iat[i]*fac_ang_dist_z))
@@ -81,20 +82,21 @@ def add_SED(ax, dataset="M87SED_EHTMWL2018", angular_scale_color=True,
         for i in range(len(df_UL)):
             ax.errorbar([df_UL["Frequency_Hz"].iat[i]*h*erg2eV], [df_UL["nuFnu_1e-12ergscm2"].iat[i]*1e-12], 
                         yerr=[df_UL["sigma_nuFnu_1e-12ergscm2"].iat[i]*1e-12], uplims=True, ls="", marker="_", 
+                        ms=ms,
                         color=cmap(norm(np.log10(df_UL['Angular_Scale_arcsec'].iat[i]*fac_ang_dist_z))), alpha=alphaUL)
         
         if colorbar:
             sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
             sm.set_array([])
-            plt.colorbar(sm, ax=ax, label=col_label, shrink=0.75, pad=0.01)
+            plt.colorbar(sm, ax=ax, label=col_label, shrink=cb_shrink, pad=cb_pad)
     else:
         ax.errorbar(df_data["Frequency_Hz"]*h*erg2eV, df_data["nuFnu_1e-12ergscm2"]*1e-12, 
                     yerr=df_data["sigma_nuFnu_1e-12ergscm2"]*1e-12, ls="", marker=marker, 
-                    c=fixed_color, label=label, alpha=alpha)
+                    c=fixed_color, ms=ms, label=label, alpha=alpha)
         if dataset=="M87SED_EHTMWL2018":
-            ax.errorbar(df_VM["Frequency_Hz"]*h*erg2eV, df_VM["nuFnu_1e-12ergscm2"]*1e-12, 
+            ax.errorbar(df_VM["Frequency_Hz"]*h*erg2eV, df_VM["nuFnu_1e-12ergscm2"]*1e-12, ms=ms,
                 yerr=df_VM["sigma_nuFnu_1e-12ergscm2"]*1e-12, ls="", marker=".", c='grey', alpha=0.3, label=label)
-        ax.errorbar(df_UL["Frequency_Hz"]*h*erg2eV, df_UL["nuFnu_1e-12ergscm2"]*1e-12, 
+        ax.errorbar(df_UL["Frequency_Hz"]*h*erg2eV, df_UL["nuFnu_1e-12ergscm2"]*1e-12, ms=ms,
                     yerr=df_UL["sigma_nuFnu_1e-12ergscm2"]*1e-12,uplims=True, ls="", marker="_", c="grey", alpha=alphaUL)
 
     if frequencymarkers:
